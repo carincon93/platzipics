c105707e75f48a4a828592fcf6f7e0f64ed072c2
Camilo Rincon <carincon93@gmail.com>
2019-08-21 00:33:19 -0500

    Configuración inicial del proyecto

---
+++ src/renderer/frontend.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', () => {
+    addImagesEvents()
+})
+
+function addImagesEvents() {
+    const thumbs = document.querySelectorAll('li.list-group-item')
+
+    for (let i = 0, length1 = thumbs.length; i < length1; i++) {
+        thumbs[i].addEventListener('click', function() {
+            changeImage(this)
+        })
+    }
+}
+
+function changeImage(node) {
+    document.querySelector('li.selected').classList.remove('selected')
+    node.classList.add('selected')
+    document.getElementById('image-displayed').src = node.querySelector('img').src
+
+}
